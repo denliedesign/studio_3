981124a53c830fa86ce6b2943fdd19c0fd34775b
denliedesign <customdenlie@gmail.com>
2023-10-03 09:46:11 -0500

    oct news

---
+++ resources/views/news/october.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+@section('title', 'News September 2023 | Studio 3 Dance Plano TX')
+@section('description', 'We’ve had a great month back to dance and hope your classes are going well...')
+@section('content')
+    <div class="banner-wrap">
+        <div class="banner"></div>
+    </div>
+
+    <div id="general-section">
+        <div class="container">
+            <h1 class="mt-5">October Newsletter</h1>
+            <h3 class="fw-bold text-center">October is Here!</h3>
+            <p class="text-center">
+                The month of October is an exciting time for Studio 3 Dance!
+                <br>Get ready to mark your calendars…
+            </p>
+            <x-envelope image="news-oct-23.png" content=""/>
+            <x-envelope image="news-oct-23-a.png" content=""/>
+            <x-envelope image="news-oct-23-b.png" content="Please join us for an enchanting Afternoon with the Snow Queen.  The event will be held at North Texas Performing Arts at Willow Bend Mall.  There will be performances, refreshments, photo and autograph opportunities with the Snow Queen and other soloists. There will be an exciting raffle for Snow Queen tickets, frames, ornaments and decorated pointe shoes. Tickets can be purchased through Eventbrite.
+<a href='https://www.eventbrite.com/e/an-afternoon-with-the-snow-queen-tickets-720562942157?aff=oddtdtcreator' target='_blank'>LINK</a>"/>
+            <x-envelope image="news-oct-23-c.png" content="Tickets go on sale October 5th for S3D’s 8th Snow Queen Ballet Production.  You will not want to miss this opportunity to see our Studio 3 Dance dancers perform this delightful ballet, based on Hans Christian Andersen’s fairy tale of the same name.  It tells the story of undying friendship between childhood friends through beautiful dance and costumes.
+<br><br><a href='https://www.eisemanncenter.com/event/i/9752/d/studio-3-dance-snow-queen' target='_blank'>Nov 11 at 7:00pm</a>
+<br><br><a href='https://www.eisemanncenter.com/event/i/9755/d/studio-3-dance-snow-queen' target='_blank'>Nov 12 at 2:00pm</a>
+"/>
+            <x-envelope image="news-oct-23-d.png" content="NEW 5 WEEK MINI MESTER CLASSES start October 18th!"/>
+            <x-envelope image="news-oct-23-e.png" content="S3D’s Spring Recitals will be held at the Eisemann Center in Richardson on
+Thursday, May 30, Friday May 31 and Saturday June 1."/>
+            <x-envelope image="news-oct-23-f.png" content="Join Studio 3 Dance in honoring one of our S3D mom this
+Breast Cancer Awareness month!
+<br><br>
+Veronica Tackett is a parent of two dancers at Studio 3 Dance and a breast cancer survivor. This past summer she was diagnosed with Ductal Carcinoma Insitu (DCIS), which is an early form of Breast Cancer. About 1 in 5 new breast cancers will be DCIS. Veronica is thankful for early detection methods and the advancements in breast cancer research.
+<br><br>
+S3D will be taking donations at the front desk that will benefit the National Breast Cancer Foundation. Studio 3 will match all donations made by our S3D families. Those who donate will receive a pink sticker and will be registered in a drawing for a shiny new PINK dance bag!  Help Studio 3 make a difference in this fight!
+<br><br>
+Dancers, please wear something PINK to class October 16-21. "/>
+            <x-envelope image="news-oct-23-g.png" content="Our 26th Anniversary Spring Recital information went out by email earlier today.
+ If you did not receive it, please check your junk or spam or contact the office
+so we can update your email.
+<br><br>
+Costume measurements will be taken in class starting October 2nd. If you are absent, we will continue to measure through the month of October."/>
+            <x-envelope image="news-oct-23-h.png" content=" Spring Recital costume deposits will be auto charged on October 15th.
+ <br>If you are unable to participate, please complete a form at the studio front desk.
+<br>We would love to have 100% participation as we celebrate our 26th season!"/>
+            <x-envelope image="news-oct-23-i.png" content="Our 4th annual Trunk or Treat celebration will be on Saturday, October 28th from 6-7:30 pm in the studio parking lot. Come for a night of prizes, food, dancing (performances by S3D company teams at 6:45), and of course, candy!
+1st, 2nd and 3rd place winners will be recognized.
+
+Use the link below to register your Trunk in the decoration contest OR to sign up to Trunk or Treat.  We want to make sure we have enough candy for all!
+<a href='https://forms.gle/8QqhtZcEUhUw2ekj9' target='_blank'>https://forms.gle/8QqhtZcEUhUw2ekj9</a>"/>
+            <x-envelope image="news-oct-23-j.png" content="“Halloweek” is later this month! Wear a costume that’s comfortable to dance in the week of October 23rd- 28th (and Monday the 30th)"/>
+            <x-envelope image="news-oct-23-k.png" content="We will be closed on Monday, October 31st for Halloween! Have a fun and spooktacular night!  If you are interested in a makeup class, please contact the front desk and we will schedule it for you."/>
+            <x-envelope image="news-oct-23-l.png" content="Studio 3 Dance would like to congratulate the following 4 dancers for their hard work in September! They are recognized for: A “can do” attitude, respect for their teachers and peers, 100% attendance, and always following dress code."/>
+            <x-envelope image="news-oct-23-m.jpg" content="Download our UPDATED APP and we can now send you text notifications! In order to identify yourself and join your performance group, you will need to log into the parent portal from the app (one time only)."/>
+
+        </div>
+    </div>
+
+@endsection

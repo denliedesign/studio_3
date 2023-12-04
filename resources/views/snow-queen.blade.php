ef64804bcfd94786c82c38d989dc383138087f9f
denliedesign <customdenlie@gmail.com>
2023-12-04 13:04:35 -0600

    snow queen 23 gallery

--- resources/views/snow-queen.blade.php
+++ resources/views/snow-queen.blade.php
@@ -8,15 +8,15 @@
     </div>
 
     <div id="teams" class="container mt-5">
-        <h1>Snow Queen 2021 Gallery</h1>
+        <h1>Snow Queen 2023 Gallery</h1>
         <div class="row my-4">
 {{--            <div class="col-sm my-2">--}}
 {{--                <img src="/images/2023-snow-queen-workshop.png" alt="snow queen workshop" class="img-fluid shadow rounded">--}}
 {{--            </div>--}}
             <div class="col-sm d-flex align-items-center my-2 text-center">
                 <div style="font-size: 1.33em;">
-                    Snow Queen 2023 will be held the weekend of November 11th and 12th at the Eisemann Bank of America Theatre in Richardson. We have openings for dancers ages 3 - 18. If you are interested in auditioning, join us August 5th or August 12th at 1:00pm.  Please call or email the studio for additional information.
-                    Rehearsals begin Friday August 18th.
+{{--                    Snow Queen 2023 will be held the weekend of November 11th and 12th at the Eisemann Bank of America Theatre in Richardson. We have openings for dancers ages 3 - 18. If you are interested in auditioning, join us August 5th or August 12th at 1:00pm.  Please call or email the studio for additional information.--}}
+{{--                    Rehearsals begin Friday August 18th.--}}
 {{--                    <br><br>--}}
 {{--                    <ul>--}}
 {{--                        <li>Saturday, August 5th at 1:00 pm.</li>--}}
@@ -30,30 +30,30 @@
 
         <section id="vision" class="container my-5">
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3">
-                <col><img src="/images/snowqueen/17-snow-queen.jpg" alt="snow queen performance red" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/18-snow-queen.jpg" alt="snow queen performance red" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/16-snow-queen.jpg" alt="snow queen performance mix" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/1-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/6-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/7-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/8-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/9-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/10-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/11-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/12-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/13-snow-queen.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/5-snow-queen.jpg" alt="snow queen performance trees" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/14-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/15-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/21-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/22-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/23-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/24-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/25-snow-queen.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/19-snow-queen.jpg" alt="snow queen performance brown" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/20-snow-queen.jpg" alt="snow queen performance brown" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/2-snow-queen.jpg" alt="snow queen performance curtain" class="img-fluid my-2"></col>
-                <col><img src="/images/snowqueen/3-snow-queen.jpg" alt="snow queen performance curtain" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-1.jpg" alt="snow queen performance red" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-2.jpg" alt="snow queen performance red" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-3.jpg" alt="snow queen performance mix" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-4.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-5.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-6.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-7.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-8.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-9.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-10.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-11.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-12.jpg" alt="snow queen performance yellow" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-13.jpg" alt="snow queen performance trees" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-14.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-15.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-16.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-17.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-18.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-19.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-20.jpg" alt="snow queen performance blue" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-21.jpg" alt="snow queen performance brown" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-22.jpg" alt="snow queen performance brown" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-23.jpg" alt="snow queen performance curtain" class="img-fluid my-2"></col>
+                <col><img src="/images/snowqueen/sq23-24.jpg" alt="snow queen performance curtain" class="img-fluid my-2"></col>
             </div>
         </section>
     </div>

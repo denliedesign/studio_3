405a4caa80cb50547fa3c76405dc7c6090d950c6
denliedesign <customdenlie@gmail.com>
2024-09-09 10:03:34 -0500

    sep news

---
+++ resources/views/news/september.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'News September 2024 | Studio 3 Dance Plano TX')
+@section('description', 'We’ve had a great month back at dance and we hope your classes are going well!  If you are looking to add a new class OR have any questions we can answer for you, please email or call the studio.  We’d love to help!')
+@section('content')
+    <div class="banner-wrap">
+        <div class="banner"></div>
+    </div>
+
+    <div id="general-section">
+        <div class="container">
+            <div class="px-md-5 d-flex justify-content-center">
+                <img src="/images/news-sep-24.png" alt="september news" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+@endsection

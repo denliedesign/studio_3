443c38e7f19e98f6e23fd538dc01d2188f394525
denliedesign <customdenlie@gmail.com>
2024-11-05 09:09:03 -0600

    nov news

---
+++ resources/views/news/november.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'News November 2024 | Studio 3 Dance Plano TX')
+@section('description', 'This month Studio 3 Dance is getting ramped up for the holiday season!')
+@section('content')
+    <div class="banner-wrap">
+        <div class="banner"></div>
+    </div>
+
+    <div id="general-section">
+        <div class="container">
+            <div class="px-md-5 d-flex justify-content-center">
+                <img src="/images/news-nov-24.jpg" alt="september news" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+@endsection
